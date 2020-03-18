9fcbbbefbd1627bdc04f5904eb0e61456c046c59
Yurii Ohiria <ohiria@yandex.com>
2020-03-18 19:28:30 +0800

    First Commit

---
+++ EvoMobileLab/EntryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EntryViewController.swift
+//  EvoMobileLab
+//
+//  Created by Yurii on 2020/3/18.
+//  Copyright © 2020 Yurii. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var noteField: UITextView!
+    
+    public var completion: ((String, String) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+    }
+    
+    @objc func didTapSave() {
+        if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
+            completion?(text, noteField.text)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EvoMobileLab/NoteViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NoteViewController.swift
+//  EvoMobileLab
+//
+//  Created by Yurii on 2020/3/18.
+//  Copyright © 2020 Yurii. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
